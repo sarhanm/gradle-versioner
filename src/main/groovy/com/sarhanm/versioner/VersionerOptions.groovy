95bea19949acc7b31cf9e4e11bb79be76904d30b
Mohammad Sarhan <mohammad@zillow.com>
2014-09-06 23:10:42 -0700

    Adding java docs to the versioner options

--- src/main/groovy/com/sarhanm/versioner/VersionerOptions.groovy
+++ src/main/groovy/com/sarhanm/versioner/VersionerOptions.groovy
@@ -7,7 +7,7 @@ package com.sarhanm.versioner
  * Example:
  *
  * build.gradle:
- *
+ *     project.extensions.create('versioner',com.sarhanm.versioner.VersionerOptions)
  *     versioner{
  *         snapshot = true
  *     }
@@ -16,11 +16,38 @@ package com.sarhanm.versioner
  * @author mohammad sarhan
  */
 class VersionerOptions {
+
+    /**
+     * Set to true to skip setting the version
+     */
     def boolean disabled = false
+
+    /**
+     * Set to true to generate a snapshot version
+     */
     def boolean snapshot = false
+
+    /**
+     * Set to true if you want to ommit all branch meta-data and only output digits.
+     */
     def boolean omitBranchMetadata = false
+
+    /**
+     * Set to the environment Key that contains the branch name.
+     * This is ONLY required for build systems other than jenkins and travis
+     */
     def String branchEnvName = null
+
+    /**
+     * The regex used to match branch names which determines which branches get
+     * a non-zero major and minor versions
+     */
     def String solidBranchRegex = Versioner.DEFAULT_SOLID_BRANCH_REGEX
+
+    /**
+     * The branch name of the common hotfix branch. Its the name of the branh
+     * that you fork from when you create a hotfix. Currently defaults to 'master'
+     */
     def String commonHotfixBranch = Versioner.DEFAULT_HOTFIX_COMMON_BRANCH
 
 }
