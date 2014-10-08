b94d0ea2abac6c124d97f9b9cd2a415e4c372d32
Mohammad Sarhan <mohammad@zillow.com>
2014-10-08 01:20:19 -0700

    Using the buildFile as a way to reference the correct directory

--- src/main/groovy/com/sarhanm/versioner/VersionerPlugin.groovy
+++ src/main/groovy/com/sarhanm/versioner/VersionerPlugin.groovy
@@ -35,7 +35,7 @@ class VersionerPlugin implements Plugin<Project>{
 
         if(!params.disabled)
         {
-            def versioner = new Versioner(params, project.buildDir)
+            def versioner = new Versioner(params, project.buildFile.parentFile)
             project.version = versioner.getVersion()
             logger.quiet "Set project '$project.name' version to : $project.version"
         }
