a6785c7a1f010aabef78029e1ae6010634592792
Mohammad Sarhan <mohammad@zillow.com>
2015-04-06 12:17:45 -0700

    Trying a way to override the api and secret

--- travis-run.sh
+++ travis-run.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+plugin_publish_keys_file=`pwd`/plugin-publish-keys.properties
+echo "gradle.publish.key=$gradlePublishKey" > ${plugin_publish_keys_file}
+echo "gradle.publish.secret=$gradlePublishSecret" >> ${plugin_publish_keys_file}
+
 # Only publish the master branch that are NOT pull requests.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    ./gradlew check bintrayUpload  publishPlugins -Dgradle.publish.key="$gradlePublishKey" -Dgradle.publish.secret="$gradlePublishSecret"
+    ./gradlew check bintrayUpload publishPlugins -Dcom.gradle.login.properties.file=$plugin_publish_keys_file
 else
     ./gradlew check
 fi
