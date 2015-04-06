567ccda8b8272916590470ae6cb3f22d747cb270
Steve Perrin <stevep@zillow.com>
2015-04-06 09:16:32 -0700

    Added Mohammad's Travis config to simplify pull request back into main gradle-versioner.

--- travis-run.sh
+++ travis-run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Only publish the master branch
-if [ "$TRAVIS_REPO_SLUG" == "steveperrin/gradle-versioner" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
-    ./gradlew check bintrayUpload
+# Only publish the master branch that are NOT pull requests.
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    ./gradlew check bintrayUpload  publishPlugins
 else
     ./gradlew check
 fi
