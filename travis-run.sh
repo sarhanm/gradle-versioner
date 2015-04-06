2307552955de9772cf3ec83d59152538d0b2c96b
Mohammad Sarhan <mohammad@zillow.com>
2015-04-06 10:39:25 -0700

    Disabling publishPlugins task

--- travis-run.sh
+++ travis-run.sh
@@ -2,7 +2,9 @@
 
 # Only publish the master branch that are NOT pull requests.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    ./gradlew check bintrayUpload  publishPlugins -Dgradle.publish.key=$gradlePublishKey -Dgradle.publish.secret=$gradlePublishSecret
+    # Disabling for now, errors downloading dependencies
+    #./gradlew check bintrayUpload  publishPlugins -Dgradle.publish.key=$gradlePublishKey -Dgradle.publish.secret=$gradlePublishSecret
+    ./gradlew check bintrayUpload
 else
     ./gradlew check
 fi
