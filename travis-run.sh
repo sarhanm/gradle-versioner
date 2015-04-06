b2ea6b0c3146016ac724b5ebffff2c710740e158
Mohammad Sarhan <mohammad@zillow.com>
2015-04-06 11:00:12 -0700

    Re-ordering repos so dependencies can be resolved

--- travis-run.sh
+++ travis-run.sh
@@ -2,9 +2,7 @@
 
 # Only publish the master branch that are NOT pull requests.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    # Disabling for now, errors downloading dependencies
-    #./gradlew check bintrayUpload  publishPlugins -Dgradle.publish.key=$gradlePublishKey -Dgradle.publish.secret=$gradlePublishSecret
-    ./gradlew check bintrayUpload
+    ./gradlew check bintrayUpload  publishPlugins -Dgradle.publish.key="$gradlePublishKey" -Dgradle.publish.secret="$gradlePublishSecret"
 else
     ./gradlew check
 fi
