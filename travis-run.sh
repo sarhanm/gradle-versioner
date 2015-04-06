09d7e845486abe67aa46d65cf2247818c221cd59
Mohammad Sarhan <mohammad@zillow.com>
2015-04-06 12:39:02 -0700

    Another way to get the env variables to be used by the plugin-publish plugin

--- travis-run.sh
+++ travis-run.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-plugin_publish_keys_file=`pwd`/plugin-publish-keys.properties
-echo "gradle.publish.key=$gradlePublishKey" > ${plugin_publish_keys_file}
-echo "gradle.publish.secret=$gradlePublishSecret" >> ${plugin_publish_keys_file}
-
 # Only publish the master branch that are NOT pull requests.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    ./gradlew check bintrayUpload publishPlugins -Dcom.gradle.login.properties.file=$plugin_publish_keys_file
+    ./gradlew check bintrayUpload publishPlugins
 else
     ./gradlew check
 fi
