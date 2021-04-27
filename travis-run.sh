093ce4e2e1a53b5d80ea1b4fb4306c401a4839a8
Mohammad Sarhan <sarhanm@gmail.com>
2021-04-27 03:24:32 -0700

    Updating to close and release staging repository

--- travis-run.sh
+++ travis-run.sh
@@ -3,7 +3,7 @@
 # Only publish the master branch that are NOT pull requests.
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     #./gradlew check jacocoTestReport coveralls bintrayUpload publishPlugins --info --stacktrace
-    ./gradlew check jacocoTestReport coveralls publishToSonatype --info --stacktrace
+    ./gradlew check jacocoTestReport coveralls publish closeAndReleaseSonatypeStagingRepository --info --stacktrace
 else
     ./gradlew check --info --stacktrace
 fi
